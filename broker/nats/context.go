f09276126bf536d46891096e21d32fc88913d97e
Haleluak <huuduc93@gmail.com>
2023-03-19 14:18:09 +0700

    Add files via upload

---
+++ broker/nats/context.go
@@ -0,0 +1,16 @@
+package nats
+
+import (
+	"context"
+	"github/haleluak/sdk/broker"
+)
+
+// setBrokerOption returns a function to setup a context with given value.
+func setBrokerOption(k, v interface{}) broker.Option {
+	return func(o *broker.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, k, v)
+	}
+}
