63b2c33331acc2209f9ce60454927cf30c6d4d94
Haleluak <huuduc93@gmail.com>
2024-01-31 16:49:25 +0700

    Create consistenthash_test.go

---
+++ algorithm/consistenthash_test.go
@@ -0,0 +1,44 @@
+package consistenthash
+
+import (
+	"strconv"
+	"testing"
+)
+
+// TestHashing tests that the hash value is distributed evenly.
+func TestHashing(t *testing.T) {
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("6", "4", "2")
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// Add a new node, 8.
+	// replicas with "hashes": 2, 4, 6, 8, 12, 14, 16, 18, 22, 24, 26, 28
+	hash.Add("8")
+
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+}
